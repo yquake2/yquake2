afe102b5340c8875e28ac4d07d5fc6961990d0ed
Yamagi Burmeister <yamagi@yamagi.org>
2009-03-03 17:38:06 +0000

    Kleinere Fixes

--- quake2/src/client/cl_input.c
+++ quake2/src/client/cl_input.c
@@ -472,7 +472,7 @@ void CL_SendCmd (void)
 
 	if ( cls.state == ca_connected)
 	{
-		if (cls.netchan.message.cursize	|| curtime - cls.netchan.last_sent > 1000 )
+		if (cls.netchan.message.cursize	|| curtime - cls.netchan.last_sent > 100 )
 			Netchan_Transmit (&cls.netchan, 0, buf.data);	
 		return;
 	}
