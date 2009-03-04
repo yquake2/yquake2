e8988c3ae06992f0330d8672660f739e0c641a27
Yamagi Burmeister <yamagi@yamagi.org>
2009-03-04 15:36:32 +0000

    Einige Dateien des Linux-Ports aufgeräumt

--- quake2/src/platforms/posix/glob.c
+++ quake2/src/platforms/posix/glob.c
@@ -47,37 +47,6 @@ static int glob_match_after_star(char *pattern, char *text)
 	}
 }
 
-#if 0 /* Not used */
-/* Return nonzero if PATTERN has any special globbing chars in it.  */
-static int glob_pattern_p(char *pattern)
-{
-	register char *p = pattern;
-	register char c;
-	int open = 0;
-
-	while ((c = *p++) != '\0')
-		switch (c) {
-		case '?':
-		case '*':
-			return 1;
-
-		case '[':		/* Only accept an open brace if there is a close */
-			open++;		/* brace to match it.  Bracket expressions must be */
-			continue;	/* complete, according to Posix.2 */
-		case ']':
-			if (open)
-				return 1;
-			continue;
-
-		case '\\':
-			if (*p++ == '\0')
-				return 0;
-		}
-
-	return 0;
-}
-#endif
-
 /* Match the pattern PATTERN against the string TEXT;
    return 1 if it matches, 0 otherwise.
 
