632ad9a1c6ca26ab82e347ab33c2186d43cf3c13
Yamagi Burmeister <yamagi@yamagi.org>
2009-06-08 16:07:56 +0000

    Versionbumb

--- quake2/src/common/qcommon.h
+++ quake2/src/common/qcommon.h
@@ -22,7 +22,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include "../game/baseq2/q_shared.h"
 
-#define	VERSION		1.03
+#define	VERSION		1.04
 #define	BASEDIRNAME	"baseq2"
 
 #if defined __linux__ 
