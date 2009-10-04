177625b311045bc69843b8648fe536f315862926
Yamagi Burmeister <yamagi@yamagi.org>
2009-10-04 14:00:56 +0000

    Version hoch auf 2.00

--- quake2/src/common/qcommon.h
+++ quake2/src/common/qcommon.h
@@ -22,7 +22,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include "../game/baseq2/q_shared.h"
 
-#define	VERSION		1.05
+#define	VERSION		2.00
 #define	BASEDIRNAME	"baseq2"
 
 #if defined __linux__ 
