86a3aa5eda435cac77f97fcfe5fc1250aae1a4b0
Yamagi Burmeister <yamagi@yamagi.org>
2010-01-28 16:56:29 +0000

    No-Op Änderung, entfernt nur sinnlosen Code. Gesehen von caedes

--- quake2/src/game/ctf/g_ai.c
+++ quake2/src/game/ctf/g_ai.c
@@ -412,12 +412,6 @@ qboolean FindTarget (edict_t *self)
 
 	if (self->monsterinfo.aiflags & AI_GOOD_GUY)
 	{
-		if (self->goalentity && self->goalentity->inuse && self->goalentity->classname)
-		{
-			if (strcmp(self->goalentity->classname, "target_actor") == 0)
-				return false;
-		}
-
 		//FIXME look for monsters?
 		return false;
 	}
