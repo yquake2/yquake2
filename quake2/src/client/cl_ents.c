755c7527f665f7b7200c2c467ff50fb5fc8b2807
Yamagi Burmeister <yamagi@yamagi.org>
2010-06-18 15:14:54 +0000

    Einen Dateiheader

--- quake2/src/client/cl_ents.c
+++ quake2/src/client/cl_ents.c
@@ -16,6 +16,11 @@
  * this program; if not, write to the Free Software Foundation, Inc., 59
  * Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
+ * =======================================================================
+ *
+ * This file implements all static entities at client site.
+ *
+ * =======================================================================
  */
 
 #include "header/client.h"
