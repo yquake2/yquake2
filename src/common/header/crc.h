88393d6cd668b3b60790d1b0a550765ef5b5bd20
Yamagi Burmeister <yamagi@yamagi.org>
2010-08-31 09:07:30 +0000

    Header guard for crc.h

--- src/common/header/crc.h
+++ src/common/header/crc.h
@@ -24,7 +24,13 @@
  * =======================================================================
  */
 
+#ifndef CO_CRC_H
+#define CO_CRC_H
+
 void CRC_Init(unsigned short *crcvalue);
 void CRC_ProcessByte(unsigned short *crcvalue, byte data);
 unsigned short CRC_Value(unsigned short crcvalue);
 unsigned short CRC_Block (byte *start, int count);
+
+#endif
+
