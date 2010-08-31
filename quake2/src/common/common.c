68ba9ae21178ccb48fe609117abce16f7e1c188a
Yamagi Burmeister <yamagi@yamagi.org>
2010-08-31 08:58:46 +0000

    Split the server side memory management into an own file

--- quake2/src/common/common.c
+++ quake2/src/common/common.c
@@ -81,65 +81,7 @@ Handles byte ordering and avoids alignment errors
 
 //===========================================================================
 
-void SZ_Init (sizebuf_t *buf, byte *data, int length)
-{
-	memset (buf, 0, sizeof(*buf));
-	buf->data = data;
-	buf->maxsize = length;
-}
 
-void SZ_Clear (sizebuf_t *buf)
-{
-	buf->cursize = 0;
-	buf->overflowed = false;
-}
-
-void *SZ_GetSpace (sizebuf_t *buf, int length)
-{
-	void	*data;
-
-	if (buf->cursize + length > buf->maxsize)
-	{
-		if (!buf->allowoverflow)
-			Com_Error (ERR_FATAL, "SZ_GetSpace: overflow without allowoverflow set");
-
-		if (length > buf->maxsize)
-			Com_Error (ERR_FATAL, "SZ_GetSpace: %i is > full buffer size", length);
-
-		SZ_Clear (buf);
-		buf->overflowed = true;
-		Com_Printf ("SZ_GetSpace: overflow\n");
-	}
-
-	data = buf->data + buf->cursize;
-	buf->cursize += length;
-
-	return data;
-}
-
-void SZ_Write (sizebuf_t *buf, void *data, int length)
-{
-	memcpy (SZ_GetSpace(buf,length),data,length);
-}
-
-void SZ_Print (sizebuf_t *buf, char *data)
-{
-	int		len;
-
-	len = (int)strlen(data)+1;
-
-	if (buf->cursize)
-	{
-		if (buf->data[buf->cursize-1])
-			memcpy ((byte *)SZ_GetSpace(buf, len),data,len); /* no trailing 0 */
-
-		else
-			memcpy ((byte *)SZ_GetSpace(buf, len-1)-1,data,len); /* write over trailing 0 */
-	}
-
-	else
-		memcpy ((byte *)SZ_GetSpace(buf, len),data,len);
-}
 
 
 //============================================================================
