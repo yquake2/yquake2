f0fa091fde42c6c3ae8abc91603fc1e7b2e25147
Yamagi Burmeister <yamagi@yamagi.org>
2010-10-28 05:57:13 +0000

    s_khz per Default auf 44

--- src/client/sound/snd_dma.c
+++ src/client/sound/snd_dma.c
@@ -121,7 +121,7 @@ S_Init ( void )
 	else
 	{
 		s_volume = Cvar_Get( "s_volume", "0.7", CVAR_ARCHIVE );
-		s_khz = Cvar_Get( "s_khz", "22", CVAR_ARCHIVE );
+		s_khz = Cvar_Get( "s_khz", "44", CVAR_ARCHIVE );
 		s_loadas8bit = Cvar_Get( "s_loadas8bit", "0", CVAR_ARCHIVE );
 		s_mixahead = Cvar_Get( "s_mixahead", "0.14", CVAR_ARCHIVE );
 		s_show = Cvar_Get( "s_show", "0", 0 );
