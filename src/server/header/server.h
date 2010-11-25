a25b89c54abaee15004b10561d72e3bae056154a
Yamagi Burmeister <yamagi@yamagi.org>
2010-11-25 16:20:12 +0000

    Lizenzsheader und Guard für server.h

--- src/server/header/server.h
+++ src/server/header/server.h
@@ -1,3 +1,32 @@
+/*
+ * Copyright (C) 1997-2001 Id Software, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ *
+ * =======================================================================
+ *
+ * Main header file for the client
+ *
+ * =======================================================================
+ */  
+
+#ifndef SV_SERVER_H
+#define SV_SERVER_H
+
 #include "../../common/header/common.h"
 #include "../../game/baseq2/game.h"
 
@@ -5,9 +34,9 @@
 #define LATENCY_COUNTS  16
 #define RATE_MESSAGES   10
 
-/* MAX_CHALLENGES is made large to prevent a denial */
-/* of service attack that could cycle all of them */
-/* out before legitimate users connected */
+/* MAX_CHALLENGES is made large to prevent a denial 
+   of service attack that could cycle all of them 
+   out before legitimate users connected */
 #define MAX_CHALLENGES  1024
 
 #define SV_OUTPUTBUF_LENGTH ( MAX_MSGLEN - 16 )
@@ -237,4 +266,6 @@ int SV_AreaEdicts ( vec3_t mins, vec3_t maxs, edict_t **list, int maxcount, int 
 int SV_PointContents ( vec3_t p );
 
 trace_t SV_Trace ( vec3_t start, vec3_t mins, vec3_t maxs, vec3_t end, edict_t *passedict, int contentmask );
+
+#endif
 
