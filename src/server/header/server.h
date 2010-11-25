3c7426f836aef2555825ed80def4304973341893
Yamagi Burmeister <yamagi@yamagi.org>
2010-11-25 15:46:36 +0000

    Verschiebe server.h in ein Unterverzeichnis

--- src/server/header/server.h
+++ src/server/header/server.h
@@ -22,8 +22,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 //define	PARANOID			// speed sapping error checking
 
-#include "../common/header/common.h"
-#include "../game/baseq2/game.h"
+#include "../../common/header/common.h"
+#include "../../game/baseq2/game.h"
 
 //=============================================================================
 
--- src/server/sv_init.c
+++ src/server/sv_init.c
@@ -18,7 +18,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 */
 
-#include "server.h"
+#include "header/server.h"
 
 server_static_t	svs;				// persistant server info
 server_t		sv;					// local server
