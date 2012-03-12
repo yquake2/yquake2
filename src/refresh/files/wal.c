48a0d6a50d26ab4035b045fd4b576aeca42b1669
Yamagi Burmeister <yamagi@yamagi.org>
2012-03-12 09:23:44 +0000

    - Reformat wal.c - Add a file header to wal.c

--- src/refresh/files/wal.c
+++ src/refresh/files/wal.c
@@ -1,18 +1,44 @@
+/*
+ * Copyright (C) 1997-2001 Id Software, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ *
+ * =======================================================================
+ *
+ * The Wal image format
+ *
+ * =======================================================================
+ */
+
 #include "../header/local.h"
 
 image_t *
-LoadWal ( char *origname )
+LoadWal(char *origname)
 {
-	miptex_t    *mt;
+	miptex_t *mt;
 	int width, height, ofs;
-	image_t     *image;
+	image_t *image;
 	int len;
 	char name[256];
 
 	/* Add the extension */
-	len = strlen( origname );
+	len = strlen(origname);
 
-	if ( strcmp( origname + len - 4, ".wal" ) )
+	if (strcmp(origname + len - 4, ".wal"))
 	{
 		strncpy(name, origname, 256);
 		strncat(name, ".wal", 255);
@@ -22,40 +48,41 @@ LoadWal ( char *origname )
 		strncpy(name, origname, 256);
 	}
 
-	ri.FS_LoadFile( name, (void **) &mt );
+	ri.FS_LoadFile(name, (void **)&mt);
 
-	if ( !mt )
+	if (!mt)
 	{
-		ri.Con_Printf( PRINT_ALL, "LoadWall: can't load %s\n", name );
-		return ( r_notexture );
+		ri.Con_Printf(PRINT_ALL, "LoadWall: can't load %s\n", name);
+		return r_notexture;
 	}
 
-	width = LittleLong( mt->width );
-	height = LittleLong( mt->height );
-	ofs = LittleLong( mt->offsets [ 0 ] );
+	width = LittleLong(mt->width);
+	height = LittleLong(mt->height);
+	ofs = LittleLong(mt->offsets[0]);
 
-	image = R_LoadPic( name, (byte *) mt + ofs, width, 0, height, 0, it_wall, 8 );
+	image = R_LoadPic(name, (byte *)mt + ofs, width, 0, height, 0, it_wall, 8);
 
-	ri.FS_FreeFile( (void *) mt );
+	ri.FS_FreeFile((void *)mt);
 
-	return ( image );
+	return image;
 }
 
-qboolean GetWalInfo (char *name, int *width, int *height)
+qboolean
+GetWalInfo(char *name, int *width, int *height)
 {
-	miptex_t	*mt;
+	miptex_t *mt;
 
-	ri.FS_LoadFile (name, (void **)&mt);
+	ri.FS_LoadFile(name, (void **)&mt);
 
 	if (!mt)
 	{
 		return false;
 	}
 
-	*width = LittleLong (mt->width);
-	*height = LittleLong (mt->height);
+	*width = LittleLong(mt->width);
+	*height = LittleLong(mt->height);
 
-	ri.FS_FreeFile ((void *)mt);
+	ri.FS_FreeFile((void *)mt);
 	return true;
 }
 
