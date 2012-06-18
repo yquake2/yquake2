e1f338f59bce5514eb789dc10570dcb63d4dbf98
Yamagi Burmeister <yamagi@yamagi.org>
2012-06-18 10:46:31 +0200

    Reformat the headers

--- src/common/header/crc.h
+++ src/common/header/crc.h
@@ -30,6 +30,6 @@
 void CRC_Init(unsigned short *crcvalue);
 void CRC_ProcessByte(unsigned short *crcvalue, byte data);
 unsigned short CRC_Value(unsigned short crcvalue);
-unsigned short CRC_Block (byte *start, int count);
+unsigned short CRC_Block(byte *start, int count);
 
 #endif
