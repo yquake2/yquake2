yquake2/yquake2

commit 1e52d35e84c90504eb72b671b76f2afbcbb07cea

Do not update mouse cursor position when paused, in menu, etc

src/backends/sdl/input.c
@@ -28,6 +28,7 @@
 #include "../../client/refresh/header/local.h"
 #include "../../client/header/keyboard.h"
 #include "../generic/header/input.h"
+#include "../../client/header/client.h"
 
 /* There's no sdl-config on OS X and Windows */
 #if defined(_WIN32) || defined(__APPLE__)
@@ -407,8 +408,10 @@ IN_Update(void)
 					SDL_WarpMouseInWindow(NULL, center_x, center_y);
 				}
 #endif
-				mouse_x += event.motion.xrel;
-				mouse_y += event.motion.yrel;
+                if (cls.key_dest == key_game && (int)cl_paused->value == 0) {
+                    mouse_x += event.motion.xrel;
+                    mouse_y += event.motion.yrel;
+                }
 				break;
 
 				/* The user pressed a button */

commit 1dc71dbd97d493ca9b6f037de58195436922ba19

Remove static keyword on some variables (these actually not static) Caused by 1e52d35e84c90504eb72b671b76f2afbcbb07cea

src/backends/sdl/input.c
@@ -84,14 +84,14 @@ cvar_t *vid_fullscreen;
 static cvar_t *in_grab;
 static cvar_t *in_mouse;
 static cvar_t *exponential_speedup;
-static cvar_t *freelook;
-static cvar_t *lookstrafe;
-static cvar_t *m_forward;
+cvar_t *freelook;
+cvar_t *lookstrafe;
+cvar_t *m_forward;
 static cvar_t *m_filter;
-static cvar_t *m_pitch;
-static cvar_t *m_side;
-static cvar_t *m_yaw;
-static cvar_t *sensitivity;
+cvar_t *m_pitch;
+cvar_t *m_side;
+cvar_t *m_yaw;
+cvar_t *sensitivity;
 static cvar_t *windowed_mouse;
 
 /* ------------------------------------------------------------------ */