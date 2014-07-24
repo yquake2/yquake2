yquake2/yquake2

commit e07294b6b12c8bb145cd9eab6f0145f597542edd

replace most strncpy calls (several of them wrong) by Q_strlcpy calls

src/common/header/shared.h
@@ -202,6 +202,7 @@ void RotatePointAroundVector(vec3_t dst,
 
 char *COM_SkipPath(char *pathname);
 void COM_StripExtension(char *in, char *out);
+char *COM_FileExtension(char *in);
 void COM_FileBase(char *in, char *out);
 void COM_FilePath(const char *in, char *out);
 void COM_DefaultExtension(char *path, const char *extension);

src/client/cl_main.c
@@ -393,7 +393,7 @@ CL_FixUpGender(void)
 			return;
 		}
 
-		strncpy(sk, skin->string, sizeof(sk) - 1);
+		Q_strlcpy(sk, skin->string, sizeof(sk));
 
 		if ((p = strchr(sk, '/')) != NULL)
 		{

src/client/cl_network.c
@@ -149,7 +149,7 @@ CL_CheckForResend(void)
 	if ((cls.state == ca_disconnected) && Com_ServerState())
 	{
 		cls.state = ca_connecting;
-		strncpy(cls.servername, "localhost", sizeof(cls.servername) - 1);
+		Q_strlcpy(cls.servername, "localhost", sizeof(cls.servername));
 		/* we don't need a challenge on the localhost */
 		CL_SendConnectPacket();
 		return;
@@ -215,7 +215,7 @@ CL_Connect_f(void)
 	CL_Disconnect();
 
 	cls.state = ca_connecting;
-	strncpy(cls.servername, server, sizeof(cls.servername) - 1);
+	Q_strlcpy(cls.servername, server, sizeof(cls.servername));
 	cls.connect_time = -99999; /* HACK: CL_CheckForResend() will fire immediately */
 }
 

src/client/cl_parse.c
@@ -843,7 +843,7 @@ CL_ParseServerData(void)
 
 	/* game directory */
 	str = MSG_ReadString(&net_message);
-	strncpy(cl.gamedir, str, sizeof(cl.gamedir) - 1);
+	Q_strlcpy(cl.gamedir, str, sizeof(cl.gamedir));
 
 	/* set gamedir */
 	if ((*str && (!fs_gamedirvar->string || !*fs_gamedirvar->string ||
@@ -902,12 +902,10 @@ CL_LoadClientinfo(clientinfo_t *ci, char *s)
 	char skin_filename[MAX_QPATH];
 	char weapon_filename[MAX_QPATH];
 
-	strncpy(ci->cinfo, s, sizeof(ci->cinfo));
-	ci->cinfo[sizeof(ci->cinfo) - 1] = 0;
+	Q_strlcpy(ci->cinfo, s, sizeof(ci->cinfo));
 
 	/* isolate the player's name */
-	strncpy(ci->name, s, sizeof(ci->name));
-	ci->name[sizeof(ci->name) - 1] = 0;
+	Q_strlcpy(ci->name, s, sizeof(ci->name));
 	t = strstr(s, "\\");
 
 	if (t)
@@ -1063,8 +1061,7 @@ CL_ParseConfigString(void)
 
 	s = MSG_ReadString(&net_message);
 
-	strncpy(olds, cl.configstrings[i], sizeof(olds));
-	olds[sizeof(olds) - 1] = 0;
+	Q_strlcpy(olds, cl.configstrings[i], sizeof(olds));
 
 	strcpy(cl.configstrings[i], s);
 
@@ -1377,7 +1374,7 @@ CL_ParseServerMessage(void)
 
 			case svc_layout:
 				s = MSG_ReadString(&net_message);
-				strncpy(cl.layout, s, sizeof(cl.layout) - 1);
+				Q_strlcpy(cl.layout, s, sizeof(cl.layout));
 				break;
 
 			case svc_playerinfo:

src/client/cl_screen.c
@@ -173,7 +173,7 @@ SCR_CenterPrint(char *str)
 	char line[64];
 	int i, j, l;
 
-	strncpy(scr_centerstring, str, sizeof(scr_centerstring) - 1);
+	Q_strlcpy(scr_centerstring, str, sizeof(scr_centerstring));
 	scr_centertime_off = scr_centertime->value;
 	scr_centertime_start = cl.time;
 

src/client/cl_view.c
@@ -282,9 +282,9 @@ CL_PrepRefresh(void)
 			/* special player weapon model */
 			if (num_cl_weaponmodels < MAX_CLIENTWEAPONMODELS)
 			{
-				strncpy(cl_weaponmodels[num_cl_weaponmodels],
+				Q_strlcpy(cl_weaponmodels[num_cl_weaponmodels],
 						cl.configstrings[CS_MODELS + i] + 1,
-						sizeof(cl_weaponmodels[num_cl_weaponmodels]) - 1);
+						sizeof(cl_weaponmodels[num_cl_weaponmodels]));
 				num_cl_weaponmodels++;
 			}
 		}

src/client/menu/menu.c
@@ -2530,10 +2530,10 @@ M_AddToServerList(netadr_t adr, char *info)
 	}
 
 	local_server_netadr[m_num_servers] = adr;
-	strncpy(local_server_names[m_num_servers], info,
-			sizeof(local_server_names[0])-1);
-	strncpy(local_server_netadr_strings[m_num_servers], s,
-			sizeof(local_server_netadr_strings[0])-1);
+	Q_strlcpy(local_server_names[m_num_servers], info,
+			sizeof(local_server_names[m_num_servers]));
+	Q_strlcpy(local_server_netadr_strings[m_num_servers], s,
+			sizeof(local_server_netadr_strings[m_num_servers]));
 	m_num_servers++;
 }
 
@@ -3960,9 +3960,10 @@ PlayerConfig_ScanDirectories(void)
 			c = b;
 		}
 
-		strncpy(s_pmi[s_numplayermodels].displayname,
-				c + 1, MAX_DISPLAYNAME - 1);
-		strcpy(s_pmi[s_numplayermodels].directory, c + 1);
+		Q_strlcpy(s_pmi[s_numplayermodels].displayname, c + 1,
+				sizeof(s_pmi[s_numplayermodels].displayname));
+		Q_strlcpy(s_pmi[s_numplayermodels].directory, c + 1,
+				sizeof(s_pmi[s_numplayermodels].directory));
 
 		FreeFileList(pcxnames, npcxfiles);
 

src/client/menu/qmenu.c
@@ -126,7 +126,14 @@ Field_Draw(menufield_s *f)
 				f->generic.name);
 	}
 
-	strncpy(tempbuffer, f->buffer + f->visible_offset, f->visible_length);
+	if (f->visible_length < sizeof(tempbuffer))
+	{
+		Q_strlcpy(tempbuffer, f->buffer + f->visible_offset, f->visible_length + 1);
+	}
+	else
+	{
+		Q_strlcpy(tempbuffer, f->buffer + f->visible_offset, sizeof(tempbuffer));
+	}
 
 	Draw_Char(f->generic.x + f->generic.parent->x + 16,
 			f->generic.y + f->generic.parent->y - 4, 18);

commit 5a7950683d351ac758d47dfab2b06ad73e7fe57a

Rewrite COM_FileExtention()
COM_FileExtension() was parsing strings from beginning to end, bailing
out as soon as '.' was found and treating everything thereafter as the
file extension. That behavior caused problem with relatives pathes like
models/monsters/tank/../ctank/skin.pcx. The new implementation uses
strrchr() to determine the last '.'.

This fixes issue #48. The bug was introduced in e07294b which replaced
hand rolled code with COM_FileExtention().

src/common/header/shared.h
@@ -202,7 +202,7 @@ void RotatePointAroundVector(vec3_t dst,
 
 char *COM_SkipPath(char *pathname);
 void COM_StripExtension(char *in, char *out);
-char *COM_FileExtension(char *in);
+const char *COM_FileExtension(const char *in);
 void COM_FileBase(char *in, char *out);
 void COM_FilePath(const char *in, char *out);
 void COM_DefaultExtension(char *path, const char *extension);

src/common/shared/shared.c
@@ -668,31 +668,17 @@ COM_StripExtension(char *in, char *out)
 	*out = 0;
 }
 
-char *
-COM_FileExtension(char *in)
+const char *
+COM_FileExtension(const char *in)
 {
-	static char exten[8];
-	int i;
+	const char *ext = strrchr(in, '.');
 
-	while (*in && *in != '.')
-	{
-		in++;
-	}
-
-	if (!*in)
+	if (!ext || ext == in)
 	{
 		return "";
 	}
 
-	in++;
-
-	for (i = 0; i < 7 && *in; i++, in++)
-	{
-		exten[i] = *in;
-	}
-
-	exten[i] = 0;
-	return exten;
+	return ext + 1;
 }
 
 void

src/game/savegame/tables/gamefunc_decs.h
@@ -73,7 +73,7 @@ extern short BigShort ( short l ) ;
 extern void COM_DefaultExtension ( char * path , const char * extension ) ;
 extern void COM_FilePath ( const char * in , char * out ) ;
 extern void COM_FileBase ( char * in , char * out ) ;
-extern char * COM_FileExtension ( char * in ) ;
+extern const char * COM_FileExtension ( const char * in ) ;
 extern void COM_StripExtension ( char * in , char * out ) ;
 extern char * COM_SkipPath ( char * pathname ) ;
 extern int Q_log2 ( int val ) ;