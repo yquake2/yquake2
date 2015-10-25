5ee1136ab59b408642d0691fce0b481e9e7ea920
Daniel Gibson <metalcaedes@gmail.com>
2015-10-25 17:34:28 +0100

    Fix duplicate input through an activated keypad.
    When the keypad was activated key presses were processed twice.
    Once as a normal char event and once as a key event (not marked
    as special). The key event to console character translation
    function turned the key event into a second character...

--- src/client/cl_keyboard.c
+++ src/client/cl_keyboard.c
@@ -209,50 +209,31 @@ CompleteCommand(void)
 void
 Key_Console(int key)
 {
+	/*
+	 * Ignore keypad in console to prevent duplicate
+	 * entries through key presses processed as a
+	 * normal char event and additionally as key
+	 * event.
+	 */
 	switch (key)
 	{
 		case K_KP_SLASH:
-			key = '/';
-			break;
 		case K_KP_MINUS:
-			key = '-';
-			break;
 		case K_KP_PLUS:
-			key = '+';
-			break;
 		case K_KP_HOME:
-			key = '7';
-			break;
 		case K_KP_UPARROW:
-			key = '8';
-			break;
 		case K_KP_PGUP:
-			key = '9';
-			break;
 		case K_KP_LEFTARROW:
-			key = '4';
-			break;
 		case K_KP_5:
-			key = '5';
-			break;
 		case K_KP_RIGHTARROW:
-			key = '6';
-			break;
 		case K_KP_END:
-			key = '1';
-			break;
 		case K_KP_DOWNARROW:
-			key = '2';
-			break;
 		case K_KP_PGDN:
-			key = '3';
-			break;
 		case K_KP_INS:
-			key = '0';
-			break;
 		case K_KP_DEL:
-			key = '.';
+			return;
 			break;
+
 		default:
 			break;
 	}
