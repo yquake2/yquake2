2e87216abfabea8deab6aaecaf1b3e52f92f2188
Valery Guskov <valerijs.gusjkovs@gmail.com>
2016-04-01 17:00:45 +0300

    Revert "ported meatball crosshair"
    This reverts commit 1e6f016fca28d76588707371e78e09af47a6fbda.

--- src/game/g_cmds.c
+++ src/game/g_cmds.c
@@ -1364,14 +1364,10 @@ ClientCommand(edict_t *ent)
 	{
 		Cmd_Wave_f(ent);
 	}
-	else if (Q_stricmp(cmd, "laser") == 0)
-	{
-		SP_LaserSight(ent);
-	}
 	else if (Q_stricmp(cmd, "playerlist") == 0)
 	{
 		Cmd_PlayerList_f(ent);
-	}	
+	}
 	else /* anything that doesn't match a command will be a chat */
 	{
 		Cmd_Say_f(ent, false, true);
