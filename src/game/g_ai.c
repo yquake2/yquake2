f3df541b7933cd5e5ed1febe11df089189d82a47
Yamagi Burmeister <yamagi@yamagi.org>
2017-09-07 18:13:01 +0200

    Fix a potential crash in ai_run_melee() and ai_run_missile().
    This was found and fixed by Maraakate.

--- src/game/g_ai.c
+++ src/game/g_ai.c
@@ -871,8 +871,10 @@ ai_run_melee(edict_t *self)
 
 	if (FacingIdeal(self))
 	{
-		self->monsterinfo.melee(self);
-		self->monsterinfo.attack_state = AS_STRAIGHT;
+		if (self->monsterinfo.melee) {
+			self->monsterinfo.melee(self);
+			self->monsterinfo.attack_state = AS_STRAIGHT;
+		}
 	}
 }
 
@@ -893,8 +895,10 @@ ai_run_missile(edict_t *self)
 
 	if (FacingIdeal(self))
 	{
-		self->monsterinfo.attack(self);
-		self->monsterinfo.attack_state = AS_STRAIGHT;
+		if (self->monsterinfo.attack) {
+			self->monsterinfo.attack(self);
+			self->monsterinfo.attack_state = AS_STRAIGHT;
+		}
 	}
 }
 
