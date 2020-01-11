bc5d9343a00fa40e52517f27327133ba3af8abcd
Denis Pauk <pauk.denis@gmail.com>
2020-01-11 16:56:42 +0200

    move headers to right place

--- src/client/refresh/vk/header/vk_local.h
+++ src/client/refresh/vk/header/vk_local.h
@@ -39,7 +39,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <vulkan/vulkan.h>
 #include <math.h>
 
-#include "../ref_shared.h"
+#include "../../ref_shared.h"
 
 #include "qvk.h"
 
