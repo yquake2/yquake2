99c6a48ef8abbd8ae87803ea6f7dd822a6237197
Denis Pauk <pauk.denis@gmail.com>
2020-01-18 15:38:48 +0200

    Cleanup create surface

--- src/client/refresh/vk/vk_light.c
+++ src/client/refresh/vk/vk_light.c
@@ -22,7 +22,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include "header/vk_local.h"
 
-int	r_dlightframecount;
+static int	r_dlightframecount;
 
 #define	DLIGHT_CUTOFF	64
 
