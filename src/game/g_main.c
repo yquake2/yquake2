079be64e1e374acc6ea193648cf52faee58aabb6
Denis Pauk <pauk.denis@gmail.com>
2020-01-26 21:54:19 +0200

    use vsnprint in Com_Printf functions

--- src/game/g_main.c
+++ src/game/g_main.c
@@ -156,7 +156,7 @@ Sys_Error(char *error, ...)
 	char text[1024];
 
 	va_start(argptr, error);
-	vsprintf(text, error, argptr);
+	vsnprintf(text, sizeof(text), error, argptr);
 	va_end(argptr);
 
 	gi.error("%s", text);
@@ -169,7 +169,7 @@ Com_Printf(char *msg, ...)
 	char text[1024];
 
 	va_start(argptr, msg);
-	vsprintf(text, msg, argptr);
+	vsnprintf(text, sizeof(text), msg, argptr);
 	va_end(argptr);
 
 	gi.dprintf("%s", text);
