5dfd9a061f1c17613f563721332424f19fe30bfc
Denis Pauk <pauk.denis@gmail.com>
2020-03-15 16:50:05 +0200

    Fix headers names

--- src/client/refresh/vk/vk_mesh.c
+++ src/client/refresh/vk/vk_mesh.c
@@ -20,7 +20,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 // vk_mesh.c: triangle model functions
 
-#include "header/vk_local.h"
+#include "header/local.h"
 
 /*
 =============================================================
