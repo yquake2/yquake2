85fb607010ee151b2d6bc217647b41fa687aa0dc
Mitchell Richters <mjr4077au@gmail.com>
2020-04-21 06:11:40 +1000

    Implement accurate-aiming CVAR in baseq2 game code.

--- src/game/g_main.c
+++ src/game/g_main.c
@@ -80,6 +80,8 @@ cvar_t *sv_maplist;
 
 cvar_t *gib_on;
 
+cvar_t *aimfix;
+
 void SpawnEntities(char *mapname, char *entities, char *spawnpoint);
 void ClientThink(edict_t *ent, usercmd_t *cmd);
 qboolean ClientConnect(edict_t *ent, char *userinfo);
