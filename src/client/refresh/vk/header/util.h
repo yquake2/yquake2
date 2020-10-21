a61325242ff08bd7264cb405dfad07a3eadb01bc
Denis Pauk <pauk.denis@gmail.com>
2020-10-21 21:36:35 +0300

    Update license in vk_util

--- src/client/refresh/vk/header/util.h
+++ src/client/refresh/vk/header/util.h
@@ -1,5 +1,6 @@
 /*
 Copyright (C) 2018 Christoph Schied
+Copyright (C) 2020 Denis Pauk
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
