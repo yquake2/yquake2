539fea96bb03fea54e179f3551da3b9d18cc75ac
Denis Pauk <pauk.denis@gmail.com>
2020-12-02 19:26:47 +0200

    add vk_retexturing force flag

--- src/client/refresh/vk/vk_image.c
+++ src/client/refresh/vk/vk_image.c
@@ -917,7 +917,8 @@ static uint32_t Vk_Upload8 (byte *data, int width, int height, imagetype_t type,
 		}
 	}
 
-	if (vk_retexturing->value)
+	// optimize 8bit images only when we forced such logic
+	if (vk_retexturing->value >= 2)
 	{
 		SmoothColorImage(trans, s, s >> 7);
 	}
@@ -976,7 +977,8 @@ Vk_LoadPic(char *name, byte *pic, int width, int realwidth,
 
 	if (bits == 8)
 	{
-		if (vk_retexturing->value)
+		// resize 8bit images only when we forced such logic
+		if (vk_retexturing->value >= 2)
 		{
 			byte *image_converted = malloc(width * height * 4);
 			scale2x(pic, image_converted, width, height);
@@ -1098,7 +1100,7 @@ Vk_LoadImage(char *name, const char* namewe, const char *ext, imagetype_t type, 
 {
 	image_t	*image = NULL;
 
-	// with retexturing and not skin
+	// with retexturing
 	if (vk_retexturing->value)
 	{
 		image = Vk_LoadHiColorImage(name, namewe, ext, type, samplerType);
