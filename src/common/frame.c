e66fd250c232e002dd61257b80352fd5fbf4cf81
Yamagi <yamagi@yamagi.org>
2021-05-25 11:54:38 +0200

    It's vk, not vulkan.

--- src/common/frame.c
+++ src/common/frame.c
@@ -240,7 +240,7 @@ static qboolean checkForHelp(int argc, char **argv)
 				printf("    'gl3'  (the OpenGL 3.2 renderer),\n");
 				printf("    'soft' (the software renderer)\n");
 #ifdef USE_REFVK
-				printf("    'vulkan' (the experimental Vulkan renderer)\n");
+				printf("    'vk' (the experimental Vulkan renderer)\n");
 #endif
 #endif // DEDICATED_ONLY
 				printf("\nSee https://github.com/yquake2/yquake2/blob/master/doc/04_cvarlist.md\nfor some more cvars\n");
