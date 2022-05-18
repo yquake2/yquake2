17e1ec7fe2a4412a7c244bae3b9b393843ff73d0
Dremor8484 <104323765+Dremor8484@users.noreply.github.com>
2022-05-18 20:18:19 +0200

    Add files via upload
    new server cvar, make possible to further customize the game (at the moment only starting health, max health and max ammo)

--- src/server/sv_main.c
+++ src/server/sv_main.c
@@ -591,6 +591,7 @@ SV_Init(void)
 	Cvar_Get("fraglimit", "0", CVAR_SERVERINFO);
 	Cvar_Get("timelimit", "0", CVAR_SERVERINFO);
 	Cvar_Get("cheats", "0", CVAR_SERVERINFO | CVAR_LATCH);
+	Cvar_Get("custom_settings", "0", CVAR_SERVERINFO | CVAR_LATCH);
 	Cvar_Get("protocol", va("%i", PROTOCOL_VERSION), CVAR_SERVERINFO | CVAR_NOSET);
 	maxclients = Cvar_Get("maxclients", "1", CVAR_SERVERINFO | CVAR_LATCH);
 	hostname = Cvar_Get("hostname", "noname", CVAR_SERVERINFO | CVAR_ARCHIVE);
