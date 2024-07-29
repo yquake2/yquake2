c92664c01de9510d570624d40999c5b1bf842d0b
Denis Pauk <pauk.denis@gmail.com>
2024-07-29 14:29:40 +0300

    collision: check string entity hash before load
    https://github.com/yquake2/yquake2remaster/issues/4#issuecomment-1741814349

--- src/common/header/common.h
+++ src/common/header/common.h
@@ -616,7 +616,7 @@ qboolean Netchan_CanReliable(netchan_t *chan);
 #include "files.h"
 
 cmodel_t *CM_LoadMap(char *name, qboolean clientload, unsigned *checksum);
-cmodel_t *CM_InlineModel(char *name);       /* *1, *2, etc */
+cmodel_t *CM_InlineModel(const char *name);       /* *1, *2, etc */
 
 int CM_NumClusters(void);
 int CM_NumInlineModels(void);
