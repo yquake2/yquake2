e8eb810c011d0edde793345fd43644c0ff0e30e2
erysdren <contact@erysdren.me>
2024-10-05 17:00:31 -0500

    SDL3: Fix inconsistent name for deleting GL context

--- src/client/refresh/gl1/gl1_sdl.c
+++ src/client/refresh/gl1/gl1_sdl.c
@@ -366,7 +366,11 @@ RI_ShutdownContext(void)
 	{
 		if(context)
 		{
+#ifdef USE_SDL3
 			SDL_GL_DestroyContext(context);
+#else
+			SDL_GL_DeleteContext(context);
+#endif
 			context = NULL;
 		}
 	}
