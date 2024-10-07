yquake2/yquake2

commit 39d2c7dbb45d53678e82baa1e727d2647d5ee755

Bump to SDL3 gitrev 8db3b47

src/client/refresh/soft/sw_main.c
@@ -2194,7 +2194,7 @@ RE_CleanFrame(void)
 	memset(swap_buffers, 0,
 		vid_buffer_height * vid_buffer_width * sizeof(pixel_t) * 2);
 
-	if (SDL_LockTexture(texture, NULL, (void**)&pixels, &pitch))
+	if (!SDL_LockTexture(texture, NULL, (void**)&pixels, &pitch))
 	{
 		Com_Printf("Can't lock texture: %s\n", SDL_GetError());
 		return;
@@ -2214,7 +2214,7 @@ RE_FlushFrame(int vmin, int vmax)
 	int pitch;
 	Uint32 *pixels;
 
-	if (SDL_LockTexture(texture, NULL, (void**)&pixels, &pitch))
+	if (!SDL_LockTexture(texture, NULL, (void**)&pixels, &pitch))
 	{
 		Com_Printf("Can't lock texture: %s\n", SDL_GetError());
 		return;

src/client/input/sdl3.c
@@ -2053,7 +2053,7 @@ IN_Controller_Init(qboolean notify_user)
 	int nummappings;
 	char controllerdb[MAX_OSPATH] = {0};
 	SDL_Joystick *joystick = NULL;
-	SDL_bool is_controller = SDL_FALSE;
+	bool is_controller = false;
 
 	cvar = Cvar_Get("in_sdlbackbutton", "1", CVAR_ARCHIVE);
 	if (cvar)
@@ -2213,7 +2213,7 @@ IN_Controller_Init(qboolean notify_user)
 #ifdef NATIVE_SDL_GYRO
 
 			if (SDL_GamepadHasSensor(controller, SDL_SENSOR_GYRO)
-				&& !SDL_SetGamepadSensorEnabled(controller, SDL_SENSOR_GYRO, SDL_TRUE) )
+				&& !SDL_SetGamepadSensorEnabled(controller, SDL_SENSOR_GYRO, true) )
 			{
 				show_gyro = true;
 				Com_Printf( "Gyro sensor enabled at %.2f Hz\n",
@@ -2224,7 +2224,7 @@ IN_Controller_Init(qboolean notify_user)
 				Com_Printf("Gyro sensor not found.\n");
 			}
 
-			SDL_bool hasLED = SDL_GetBooleanProperty(SDL_GetGamepadProperties(controller), SDL_PROP_JOYSTICK_CAP_RGB_LED_BOOLEAN, SDL_FALSE);
+			bool hasLED = SDL_GetBooleanProperty(SDL_GetGamepadProperties(controller), SDL_PROP_JOYSTICK_CAP_RGB_LED_BOOLEAN, false);
 			if (hasLED)
 			{
 				SDL_SetGamepadLED(controller, 0, 80, 0);	// green light
@@ -2248,7 +2248,7 @@ IN_Controller_Init(qboolean notify_user)
 				show_haptic = true;
 			}
 
-			SDL_bool hasRumble = SDL_GetBooleanProperty(SDL_GetGamepadProperties(controller), SDL_PROP_GAMEPAD_CAP_RUMBLE_BOOLEAN, SDL_FALSE);
+			bool hasRumble = SDL_GetBooleanProperty(SDL_GetGamepadProperties(controller), SDL_PROP_GAMEPAD_CAP_RUMBLE_BOOLEAN, false);
 			if (hasRumble)
 			{
 				show_haptic = true;

src/client/vid/glimp_sdl3.c
@@ -124,7 +124,7 @@ CreateSDLWindow(int flags, int fullscreen, int w, int h)
 	SDL_SetNumberProperty(props, "flags", flags);
 
 	if (flags & SDL_WINDOW_OPENGL)
-		SDL_SetBooleanProperty(props, SDL_PROP_WINDOW_CREATE_OPENGL_BOOLEAN, SDL_TRUE);
+		SDL_SetBooleanProperty(props, SDL_PROP_WINDOW_CREATE_OPENGL_BOOLEAN, true);
 
 	window = SDL_CreateWindowWithProperties(props);
 	SDL_DestroyProperties(props);
@@ -161,7 +161,7 @@ CreateSDLWindow(int flags, int fullscreen, int w, int h)
 			   switch to it in exclusive fullscreen mode. */
 			SDL_DisplayMode closestMode;
 
-			if (SDL_GetClosestFullscreenDisplayMode(last_display, w, h, vid_rate->value, false, &closestMode) != SDL_TRUE)
+			if (SDL_GetClosestFullscreenDisplayMode(last_display, w, h, vid_rate->value, false, &closestMode) != true)
 			{
 				Com_Printf("SDL was unable to find a mode close to %ix%i@%f\n", w, h, vid_rate->value);
 
@@ -169,7 +169,7 @@ CreateSDLWindow(int flags, int fullscreen, int w, int h)
 				{
 					Com_Printf("Retrying with desktop refresh rate\n");
 
-					if (SDL_GetClosestFullscreenDisplayMode(last_display, w, h, vid_rate->value, false, &closestMode) == SDL_TRUE)
+					if (SDL_GetClosestFullscreenDisplayMode(last_display, w, h, vid_rate->value, false, &closestMode) == true)
 					{
 						Cvar_SetValue("vid_rate", 0);
 					}
@@ -509,7 +509,7 @@ GLimp_InitGraphics(int fullscreen, int *pwidth, int *pheight)
 		{
 			if (fullscreen == FULLSCREEN_EXCLUSIVE)
 			{
-				if (SDL_GetClosestFullscreenDisplayMode(last_display, width, height, vid_rate->value, false, &closestMode) != SDL_TRUE)
+				if (SDL_GetClosestFullscreenDisplayMode(last_display, width, height, vid_rate->value, false, &closestMode) != true)
 				{
 					Com_Printf("SDL was unable to find a mode close to %ix%i@%f\n", width, height, vid_rate->value);
 
@@ -517,7 +517,7 @@ GLimp_InitGraphics(int fullscreen, int *pwidth, int *pheight)
 					{
 						Com_Printf("Retrying with desktop refresh rate\n");
 
-						if (SDL_GetClosestFullscreenDisplayMode(last_display, width, height, 0, false, &closestMode) == SDL_TRUE)
+						if (SDL_GetClosestFullscreenDisplayMode(last_display, width, height, 0, false, &closestMode) == true)
 						{
 							Cvar_SetValue("vid_rate", 0);
 						}
@@ -764,16 +764,16 @@ GLimp_GrabInput(qboolean grab)
 {
 	if(window != NULL)
 	{
-		SDL_SetWindowMouseGrab(window, grab ? SDL_TRUE : SDL_FALSE);
+		SDL_SetWindowMouseGrab(window, grab ? true : false);
 	}
 
 #ifdef USE_SDL3
-	if(!SDL_SetWindowRelativeMouseMode(window, grab ? SDL_TRUE : SDL_FALSE))
+	if(!SDL_SetWindowRelativeMouseMode(window, grab ? true : false))
 	{
 		Com_Printf("WARNING: Setting Relative Mousemode failed, reason: %s\n", SDL_GetError());
 	}
 #else
-	if(SDL_SetWindowRelativeMouseMode(window, grab ? SDL_TRUE : SDL_FALSE) < 0)
+	if(SDL_SetWindowRelativeMouseMode(window, grab ? true : false) < 0)
 	{
 		Com_Printf("WARNING: Setting Relative Mousemode failed, reason: %s\n", SDL_GetError());
 		Com_Printf("         You should probably update to SDL 2.0.3 or newer!\n");

commit 14db96211bb917089ec811299ac391e10b039ead

soft: fix texture lock
Regression is introduced in 39d2c7dbb45d53678e82baa1e727d2647d5ee755
as sdl3 has changed logic of SDL_LockTexture function call.

src/client/refresh/soft/sw_main.c
@@ -2194,7 +2194,11 @@ RE_CleanFrame(void)
 	memset(swap_buffers, 0,
 		vid_buffer_height * vid_buffer_width * sizeof(pixel_t) * 2);
 
+#ifdef USE_SDL3
 	if (!SDL_LockTexture(texture, NULL, (void**)&pixels, &pitch))
+#else
+	if (SDL_LockTexture(texture, NULL, (void**)&pixels, &pitch))
+#endif
 	{
 		Com_Printf("Can't lock texture: %s\n", SDL_GetError());
 		return;
@@ -2214,7 +2218,11 @@ RE_FlushFrame(int vmin, int vmax)
 	int pitch;
 	Uint32 *pixels;
 
+#ifdef USE_SDL3
 	if (!SDL_LockTexture(texture, NULL, (void**)&pixels, &pitch))
+#else
+	if (SDL_LockTexture(texture, NULL, (void**)&pixels, &pitch))
+#endif
 	{
 		Com_Printf("Can't lock texture: %s\n", SDL_GetError());
 		return;