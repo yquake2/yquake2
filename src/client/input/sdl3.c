4f52c04b3b81ef426b7ab60b7618ab0e3997fc5a
BjossiAlfreds <bjornalfredsson@yahoo.com>
2024-10-26 19:56:02 +0000

    Small fix for SDL3 API change for SetClipboardText

--- src/client/input/sdl3.c
+++ src/client/input/sdl3.c
@@ -2437,8 +2437,13 @@ IN_GetClipboardText(char *out, size_t n)
 	SDL_free(s);
 }
 
+/* Copy string s to the clipboard.
+   Returns 0 on success, 1 otherwise.
+*/
 int
 IN_SetClipboardText(const char *s)
 {
-	return SDL_SetClipboardText(s);
+	bool res = SDL_SetClipboardText(s);
+
+	return !res;
 }
