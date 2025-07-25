3c6be2069dab98c47aa21e844ee9d5a6baca5685
Denis Pauk <pauk.denis@gmail.com>
2025-07-26 01:11:34 +0300

    soft: use nontransprent white as default palette

--- src/client/refresh/soft/sw_alias.c
+++ src/client/refresh/soft/sw_alias.c
@@ -737,11 +737,11 @@ void R_PolysetDrawSpansConstant8_66(const entity_t *currententity, spanpackage_t
 
 /*
 ================
-R_AliasDrawModel
+R_DrawAliasModel
 ================
 */
 void
-R_AliasDrawModel(entity_t *currententity, const model_t *currentmodel)
+R_DrawAliasModel(entity_t *currententity, const model_t *currentmodel)
 {
 	s_pmdl = (dmdl_t *)currentmodel->extradata;
 
@@ -791,7 +791,7 @@ R_AliasDrawModel(entity_t *currententity, const model_t *currentmodel)
 	// set up the skin and verify it exists
 	if ( !R_AliasSetupSkin(currententity, currentmodel) )
 	{
-		R_Printf(PRINT_ALL, "R_AliasDrawModel %s: NULL skin found\n",
+		R_Printf(PRINT_ALL, "R_DrawAliasModel %s: NULL skin found\n",
 			currentmodel->name);
 		aliasxscale = oldAliasxscale;
 		aliasyscale = oldAliasyscale;
