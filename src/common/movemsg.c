yquake2/yquake2remaster

movemsg.c: WriteDeltaEntity uses nullstate internally
https://github.com/yquake2/yquake2/issues/1256

src/common/movemsg.c
@@ -192,6 +192,8 @@ vec3_t bytedirs[NUMVERTEXNORMALS] = {
 	{-0.688191, -0.587785, -0.425325}
 };
 
+static const entity_xstate_t es_nullstate = {0};
+
 size_t
 MSG_ConfigString_Size(const char *s)
 {
@@ -386,7 +388,19 @@ MSG_DeltaEntity_Size(const entity_xstate_t *from, const entity_xstate_t *to,
 	qboolean force, qboolean newentity, int protocol)
 {
 	size_t sz;
-	int bits = DeltaEntityBits(from, to, newentity, protocol);
+	int bits;
+
+	if (!from)
+	{
+		from = &es_nullstate;
+	}
+
+	if (!to)
+	{
+		to = &es_nullstate;
+	}
+
+	bits = DeltaEntityBits(from, to, newentity, protocol);
 
 	if (!bits && !force)
 	{
@@ -856,10 +870,14 @@ MSG_WriteDeltaEntity(const entity_xstate_t *from,
 {
 	int bits;
 
-	if (!to->number)
+	if (!from)
 	{
-		Com_Error(ERR_FATAL, "Unset entity number");
-		return;
+		from = &es_nullstate;
+	}
+
+	if (!to)
+	{
+		to = &es_nullstate;
 	}
 
 	/* entnums are sent in 16-bit form */

src/server/sv_entities.c
@@ -40,11 +40,9 @@ static void
 SV_EmitPacketEntities(client_frame_t *from, client_frame_t *to, sizebuf_t *msg,
 	int protocol)
 {
-	entity_xstate_t nullstate, *oldent, *newent;
+	entity_xstate_t *oldent, *newent;
 	int oldindex, newindex;
 	int from_num_entities;
-
-	memset(&nullstate, 0, sizeof(nullstate));
 
 	MSG_WriteByte(msg, svc_packetentities);
 
@@ -112,7 +110,7 @@ SV_EmitPacketEntities(client_frame_t *from, client_frame_t *to, sizebuf_t *msg,
 		{
 			/* this is a new entity, send it from the baseline */
 			MSG_WriteDeltaEntity(
-				(newnum < sv.numbaselines) ? &sv.baselines[newnum] : &nullstate,
+				(newnum < sv.numbaselines) ? &sv.baselines[newnum] : NULL,
 				newent, msg, true, true, protocol);
 
 			newindex++;

yquake2/yquake2

src/common/movemsg.c
@@ -192,6 +192,8 @@ vec3_t bytedirs[NUMVERTEXNORMALS] = {
 	{-0.688191, -0.587785, -0.425325}
 };
 
+static const entity_state_t es_nullstate = {0};
+
 size_t
 MSG_ConfigString_Size(const char *s)
 {
@@ -371,7 +373,19 @@ MSG_DeltaEntity_Size(const entity_state_t *from, const entity_state_t *to,
 	qboolean force, qboolean newentity)
 {
 	size_t sz;
-	int bits = DeltaEntityBits(from, to, newentity);
+	int bits;
+
+	if (!from)
+	{
+		from = &es_nullstate;
+	}
+
+	if (!to)
+	{
+		to = &es_nullstate;
+	}
+
+	bits = DeltaEntityBits(from, to, newentity);
 
 	if (!bits && !force)
 	{
@@ -780,6 +794,16 @@ MSG_WriteDeltaEntity(const entity_state_t *from,
 		qboolean newentity)
 {
 	int bits;
+
+	if (!from)
+	{
+		from = &es_nullstate;
+	}
+
+	if (!to)
+	{
+		to = &es_nullstate;
+	}
 
 	/* entnums are sent in 16-bit form */
 	if (to->number > SHRT_MAX)

src/server/sv_entities.c
@@ -39,11 +39,9 @@ static YQ2_ALIGNAS_TYPE(int32_t) byte fatpvs[65536 / 8];
 static void
 SV_EmitPacketEntities(client_frame_t *from, client_frame_t *to, sizebuf_t *msg)
 {
-	entity_state_t nullstate, *oldent, *newent;
+	entity_state_t *oldent, *newent;
 	int oldindex, newindex;
 	int from_num_entities;
-
-	memset(&nullstate, 0, sizeof(nullstate));
 
 	MSG_WriteByte(msg, svc_packetentities);
 
@@ -111,7 +109,7 @@ SV_EmitPacketEntities(client_frame_t *from, client_frame_t *to, sizebuf_t *msg)
 		{
 			/* this is a new entity, send it from the baseline */
 			MSG_WriteDeltaEntity(
-				(newnum < sv.numbaselines) ? &sv.baselines[newnum] : &nullstate,
+				(newnum < sv.numbaselines) ? &sv.baselines[newnum] : NULL,
 				newent, msg, true, true);
 
 			newindex++;