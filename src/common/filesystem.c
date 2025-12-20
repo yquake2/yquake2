ba6f403566003f826686f7649914a7e929cec9cf
ABalfoort <63945418+abalfoort@users.noreply.github.com>
2025-12-20 16:23:45 +0100

    Fix build warnings (#1271)
    Fix warnings:
    
    * According to cpp size_t is a unsigned integer
    * According to linux compiler size_t is a long unsigned int
    * According to windows compiler size_t is a long long unsigned int
    * Use Com_sprintf
    
    ---------
    
    Co-authored-by: abalfoort <solydxk.abalfoort@tuta.com>

--- src/common/filesystem.c
+++ src/common/filesystem.c
@@ -1700,7 +1700,7 @@ FS_FileInGamedir(const char *file)
 			return false;     /* couldn't find one anywhere */
 		}
 
-		snprintf(name, MAX_OSPATH, "%s/%s", path, file);
+		Com_sprintf(name, MAX_OSPATH, "%s/%s", path, file);
 
 		if ((fd = Q_fopen(name, "rb")) != NULL)
 		{
--- src/server/sv_init.c
+++ src/server/sv_init.c
@@ -108,7 +108,7 @@ SV_FindIndex(const char *name, int start, int max, qboolean create)
 		{
 			if (sv.state != ss_loading)
 			{
-				Com_Printf("Failed to load %s: configstring too long: %u > %u\n",
+				Com_Printf("Failed to load %s: configstring too long: " YQ2_COM_PRIdS " > " YQ2_COM_PRIdS "\n",
 					name, len, space - 1);
 			}
 
