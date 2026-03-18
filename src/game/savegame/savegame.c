d2c08d5a93dab40d181b1bbae14e7a21a09c4bbd
BjossiAlfreds <bjornalfredsson@yahoo.com>
2026-03-18 16:18:43 +0000

    savegame: Sanitize client structs after reading from save

--- src/game/savegame/savegame.c
+++ src/game/savegame/savegame.c
@@ -812,6 +812,24 @@ WriteClient(FILE *f, gclient_t *client)
  * Read the client struct from a file
  */
 static void
+SanitizeClientStruct(gclient_t *cl)
+{
+	client_persistant_t *p;
+
+	p = &cl->pers;
+	p->userinfo[sizeof(p->userinfo) - 1] = 0;
+	p->netname[sizeof(p->netname) - 1] = 0;
+
+	p = &cl->resp.coop_respawn;
+	p->userinfo[sizeof(p->userinfo) - 1] = 0;
+	p->netname[sizeof(p->netname) - 1] = 0;
+
+	ValidateSelectedItem(cl);
+	cl->ammo_index = GetWeaponAmmoIndex(cl->pers.weapon);
+	cl->flood_whenhead = 0;
+}
+
+static void
 ReadClient(FILE *f, gclient_t *client, short save_ver)
 {
 	field_t *field;
@@ -825,6 +843,8 @@ ReadClient(FILE *f, gclient_t *client, short save_ver)
 			ReadField(f, field, (byte *)client);
 		}
 	}
+
+	SanitizeClientStruct(client);
 
 	if (save_ver < 3)
 	{
