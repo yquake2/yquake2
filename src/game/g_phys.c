6587a5759f0b67c7944b32ab2929aface4703aba
BjossiAlfreds <bjornalfredsson@yahoo.com>
2026-04-19 10:15:20 +0000

    game: Bad entity movetype no longer drops the server

--- src/game/g_phys.c
+++ src/game/g_phys.c
@@ -1301,6 +1301,10 @@ G_RunEntity(edict_t *ent)
 			SV_Physics_Toss(ent);
 			break;
 		default:
-			gi.error("%s: bad movetype %i", __func__, (int)ent->movetype);
+			gi.dprintf("%s:%d has bad movetype: %d\n",
+				ent->classname, ent->s.number, ent->movetype);
+			ent->movetype = MOVETYPE_NONE;
+			SV_Physics_None(ent);
+			break;
 	}
 }
