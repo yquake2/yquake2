69391d29129ea98469068fd196b265cce94cf1b6
BjossiAlfreds <bjornalfredsson@yahoo.com>
2026-06-01 14:43:49 +0000

    game: Chick uses sound_fall_down in death2 move
    This sound was being precached but not used. Was almost definitely intended for this move

--- src/game/monster/chick/chick.c
+++ src/game/monster/chick/chick.c
@@ -454,6 +454,18 @@ chick_dead(edict_t *self)
 	gi.linkentity(self);
 }
 
+static void
+chick_fall_down(edict_t *self)
+{
+	if (!self)
+	{
+		return;
+	}
+
+	/* CHAN_VOICE so gibbing the body cancels this sound */
+	gi.sound(self, CHAN_VOICE, sound_fall_down, 1, ATTN_NORM, 0);
+}
+
 static mframe_t chick_frames_death2[] = {
 	{ai_move, -6, NULL},
 	{ai_move, 0, NULL},
@@ -476,7 +488,7 @@ static mframe_t chick_frames_death2[] = {
 	{ai_move, -5, NULL},
 	{ai_move, 4, NULL},
 	{ai_move, 15, NULL},
-	{ai_move, 14, NULL},
+	{ai_move, 14, chick_fall_down},
 	{ai_move, 1, NULL}
 };
 
